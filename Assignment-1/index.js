987e1d0139060ae4618f7f90ba136be3488b594f
Mohammed Hosny <mohammedhosny94@hotmail.com>
2020-09-10 01:50:26 +0200

    Add Assignment-1

---
+++ Assignment-1/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger=require('morgan');
+const dishRouter = require('./dishRouter');
+const promoRouter = require('./promoRouter');
+const leaderRouter = require('./leaderRouter');
+     
+const hostname = 'localhost';
+const port = 3000;
+const app = express();
+app.use(logger("dev"));
+app.use(express.static(__dirname+'/public'));
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
+
+app.listen(port,hostname,()=>{
+  console.log("Server running at http://"+hostname+":"+port);
+});
