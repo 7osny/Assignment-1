f875ceb0ea1298322e59bb4476d614b6a502bb64
Mohammed Hosny <mohammedhosny94@hotmail.com>
2020-11-24 03:08:53 +0200

    Assignment 3

---
+++ Assignment-3/routes/promoRouter.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+const promotions = require('../models/Promotions');
+const Promotions = require('../models/Promotions');
+const authenticate= require('../authenticate');
+const promoRouter =	express.Router();
+promoRouter.use(bodyParser.json());
+
+
+promoRouter.route('/')
+.get((req,res,next) => {
+    promotions.find({})
+     .then((promo)=>{
+         res.statusCode=200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(promo);
+     },(err)=>next(err))
+     .catch((err)=>next(err));
+ })
+ .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+     promotions.create(req.body)
+     .then((promo)=>{
+         res.statusCode=200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(promo);
+     },(err)=>next(err))
+     .catch((err)=>next(err));
+ })
+ .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+     res.statusCode = 403;
+     res.end('PUT operation not supported on /promo');
+ })
+ .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+     promotions.remove({})
+     .then((resp) => {
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(resp);
+     }, (err) => next(err))
+     .catch((err) => next(err));    
+ });
+ 
+ promoRouter.route('/:promoId')
+ .get((req,res,next) => {
+     Promotions.findById(req.params.promoId)
+     .then((promo) => {
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(promo);
+     }, (err) => next(err))
+     .catch((err) => next(err));
+ })
+ .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+     res.statusCode = 403;
+     res.end('POST operation not supported on /promo/'+ req.params.promoId);
+ })
+ .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+     promotions.findByIdAndUpdate(req.params.promoId, {
+         $set: req.body
+     }, { new: true })
+     .then((promo) => {
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(promo);
+     }, (err) => next(err))
+     .catch((err) => next(err));
+ })
+ .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+     promotions.findByIdAndRemove(req.params.promoId)
+     .then((resp) => {
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.json(resp);
+     }, (err) => next(err))
+     .catch((err) => next(err));
+ });
+module.exports=promoRouter;
